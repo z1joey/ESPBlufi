c4c664b4b9154dae114c772ff4327b0387fd2c96
Joey Zhang <joey.zhang@tgitech.net>
2020-10-22 17:56:39 +0800

    wip: Wifi things

--- ESPBluefiSample/DetailViewController.swift
+++ ESPBluefiSample/DetailViewController.swift
@@ -17,6 +17,17 @@ class DetailViewController: UIViewController {
     public var device: ESPPeripheral?
     public var isConnected: Bool = false
 
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        tableView.dataSource = self
+//    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let sta = segue.destination as? STAViewController {
+            sta.paramsDelegate = self
+        }
+    }
+
     @IBAction func connectAction(_ sender: Any) {
         guard device != nil else { return }
 
@@ -25,7 +36,7 @@ class DetailViewController: UIViewController {
 
         blufiClient = BlufiClient()
         blufiClient?.centralManagerDelete = self
-        blufiClient?.peripheralDelegate = self
+        //blufiClient?.peripheralDelegate = self
         blufiClient?.blufiDelegate = self
         blufiClient?.connect(device!.uuid.uuidString)
     }
@@ -36,17 +47,9 @@ class DetailViewController: UIViewController {
 
     @IBAction func bluefiAction(_ sender: Any) {
     }
-}
-
-extension DetailViewController: CBCentralManagerDelegate, CBPeripheralDelegate, BlufiDelegate {
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-    }
-
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        updateMessage("Connected device")
-    }
 
     private func updateMessage(_ message: String) {
+        messages.append(message)
         let indexPath = IndexPath(row: messages.count - 1, section: 0)
         tableView.beginUpdates()
         tableView.insertRows(at: [indexPath], with: .none)
@@ -55,13 +58,92 @@ extension DetailViewController: CBCentralManagerDelegate, CBPeripheralDelegate, 
     }
 }
 
+// MARK: - CBCentralManagerDelegate
+extension DetailViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        updateMessage("Connected device")
+    }
+
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        updateMessage("Connecting device failed")
+    }
+
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        updateMessage("Disconnected device")
+    }
+}
+
+// MARK: - BlufiDelegate
+extension DetailViewController: BlufiDelegate {
+    func blufi(_ client: BlufiClient, gattPrepared status: BlufiStatusCode, service: CBService?, writeChar: CBCharacteristic?, notifyChar: CBCharacteristic?) {
+        if status == StatusSuccess {
+            isConnected = true
+            updateMessage("BluFi connection has prepared")
+        } else {
+            blufiClient?.close()
+            if service == nil {
+                updateMessage("Discovering services failed")
+            }
+            if writeChar == nil {
+                updateMessage("Discovering write char failed")
+            }
+            if notifyChar == nil {
+                updateMessage("Discovering notify char failed")
+            }
+        }
+    }
+
+    func blufi(_ client: BlufiClient, didPostConfigureParams status: BlufiStatusCode) {
+        if status == StatusSuccess {
+            updateMessage("Post configure params complete")
+        } else {
+            updateMessage("Post configure params failed: \(status)")
+        }
+    }
+
+    func blufi(_ client: BlufiClient, didReceiveDeviceStatusResponse response: BlufiStatusResponse?, status: BlufiStatusCode) {
+        if status == StatusSuccess, response != nil {
+            updateMessage("Receive device status:\n\(response!.getStatusInfo())")
+        } else {
+            updateMessage("Receive device status error: \(status)")
+        }
+    }
+
+//    func blufi(_ client: BlufiClient, didReceiveDeviceScanResponse scanResults: [BlufiScanResponse]?, status: BlufiStatusCode) {
+//        if status == StatusSuccess {
+//            var info = String()
+//            info.append(contentsOf: "Receive device scan results:\n")
+//
+//            for response in scanResults ?? [] {
+//                info.append(contentsOf: "SSID: \(response.ssid), RSSI: \(response.rssi)\n")
+//            }
+//
+//            updateMessage(info)
+//        } else {
+//            updateMessage("Receive device scan results error: \(status)")
+//        }
+//    }
+}
+
+// MARK: - ConfigureParamsDelegate
+extension DetailViewController: ConfigureParamsDelegate {
+    func didSetParams(params: BlufiConfigureParams) {
+        blufiClient?.configure(params)
+    }
+}
+
+// MARK: - UITableViewDataSource
 extension DetailViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row]
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return messages.count
     }
 }
---
+++ ESPBluefiSample/STAViewController.swift
@@ -0,0 +1,90 @@
+//
+//  STAViewController.swift
+//  ESPBluefiSample
+//
+//  Created by joey on 10/22/20.
+//  Copyright © 2020 tecpal. All rights reserved.
+//
+
+import UIKit
+import SystemConfiguration.CaptiveNetwork
+import CoreLocation
+
+class STAViewController: UIViewController {
+    @IBOutlet weak var ssidTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    private var locationManager: CLLocationManager?
+
+    public weak var paramsDelegate: ConfigureParamsDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //if getUserLocationAuth() {
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestAlwaysAuthorization()
+        //}
+    }
+
+    @IBAction func saveAction(_ sender: Any) {
+        guard let ssid = ssidTextField.text, let password = passwordTextField.text else { return }
+
+        let params = BlufiConfigureParams()
+        params.opMode = OpModeSta
+        params.staSsid = ssid
+        params.staPassword = password
+
+        paramsDelegate?.didSetParams(params: params)
+        navigationController?.popViewController(animated: true)
+    }
+
+    private func getSSID() -> String? {
+        guard let interface = (CNCopySupportedInterfaces() as? [String])?.first,
+            let unsafeInterfaceData = CNCopyCurrentNetworkInfo(interface as CFString) as? [String: Any],
+            let ssid = unsafeInterfaceData["SSID"] as? String else{
+                return nil
+        }
+        return ssid
+    }
+
+//    private func getUserLocationAuth() -> Bool {
+//        switch CLLocationManager.authorizationStatus() {
+//        case .notDetermined, .restricted, .denied:
+//            return false
+//        case .authorizedAlways, .authorizedWhenInUse:
+//            return true
+//        default:
+//            return false
+//        }
+//    }
+}
+
+extension STAViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        var shouldShowAlert = false
+
+        switch status {
+        case .notDetermined, .restricted, .authorizedAlways, .authorizedWhenInUse:
+            break
+        case .denied:
+            shouldShowAlert = true
+            break
+        default:
+            break
+        }
+
+        if shouldShowAlert {
+            print("should go to setting")
+        } else {
+            ssidTextField.text = getSSID()
+        }
+    }
+}
+
+public protocol ConfigureParamsDelegate: class {
+    func didSetParams(params: BlufiConfigureParams)
+}
